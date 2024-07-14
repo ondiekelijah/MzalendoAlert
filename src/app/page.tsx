fd4c76f283f62ed0caaba74af290edff163a11b6
Ondiek Elijah Ochieng <oochieng@safaricom.co.ke>
2024-07-15 02:10:54 +0300

    deployment: deploying to prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -307,7 +307,7 @@ export default function Home() {
       {isModalVisible && (
         <div
           id="popup-modal"
-          tabIndex="-1"
+          tabIndex={-1}
           className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full bg-gray-800 bg-opacity-50"
         >
           <div className="relative p-4 w-full max-w-md">
@@ -366,7 +366,7 @@ export default function Home() {
                   type="button"
                   className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
                 >
-                  Yes, I'm sure
+                  Yes, I&apos;m sure{" "}
                 </button>
               </div>
             </div>
