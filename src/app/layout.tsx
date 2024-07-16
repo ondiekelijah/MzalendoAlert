6148416e5dbb65556afa42a43c40ed010b0b9097
Ondiek Elijah Ochieng <oochieng@safaricom.co.ke>
2024-07-16 07:35:39 +0300

    chore: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,15 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "MzalendoAlert: Help Find Missing Persons Through Tweets",
   description: "Support our efforts to locate missing loved ones. Post tweets, find helpful links, and get guidance on what to do when someone goes missing. Your tweets can bring someone home.",
-  metadataBase: new URL("https://www.mzalendoalert.com"),
-  openGraph: {
-    title: "MzalendoAlert: Help Find Missing Persons Through Tweets",
-    description: "Support our efforts to locate missing loved ones. Post tweets, find helpful links, and get guidance on what to do when someone goes missing. Your tweets can bring someone home.",
-    url: "https://www.mzalendoalert.com",
-    siteName: "MzalendoAlert",
-    locale: "en_US",
-    type: "website",
-  },
 };
 
 export default function RootLayout({
