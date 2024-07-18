295166b4fdbb13e29135d12809877c61b26114a3
Ondiek Elijah Ochieng <oochieng@safaricom.co.ke>
2024-07-18 16:54:25 +0300

    feat: round tweet card bottom

--- src/app/components/MissingPersonsDisplay.tsx
+++ src/app/components/MissingPersonsDisplay.tsx
@@ -19,15 +19,15 @@ const MissingPersonsDisplay: React.FC<MissingPersonsProps> = ({
       </p>
     </div>
 
-    <div className="mt-2 grid gap-4 text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-2 lg:text-left">
+    <div className="mt-2 grid gap-3 text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-2 lg:text-left">
       {tweets.map((tweet) => (
         <div
           key={tweet.tweetId}
-          className="relative h-96 w-4/5 m-auto overflow-hidden"
+          className="relative h-96 w-4/5 m-auto overflow-hidden rounded-b-xl shadow-lg"
         >
           {" "}
           <Tweet id={tweet.tweetId} />
-          <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
+          <div className="absolute  bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
         </div>
       ))}
     </div>
